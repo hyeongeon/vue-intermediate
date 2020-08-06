cc54063d0982be479326f4b1a2a658aceec350e1
황형언 <donkey__@daou.co.kr>
2020-08-06 10:31:07 +0900

    Todo component 구현

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <input v-model="newTodoItem" type="text" />
+      <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            newTodoItem: ""
+        }
+    },
+
+    methods: {
+        addTodo: function() {
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.newTodoItem = "";
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
