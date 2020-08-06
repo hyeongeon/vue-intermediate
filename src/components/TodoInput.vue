0658c47e385e5a2b14142de8f8ffefb2d3979359
hyeongeon Hwang <hyeongeonhwang@hyeongeonui-MacBookPro.local>
2020-08-06 23:20:32 +0900

    todoinput, todolist

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,27 +1,49 @@
 <template>
   <div>
-      <input v-model="newTodoItem" type="text" />
-      <button v-on:click="addTodo">add</button>
+    <input class="inputBox" v-model="newTodoItem" type="text" v-on:keyup.enter="addTodo" />
+    <button class="addBtn" v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
-    data: function() {
-        return {
-            newTodoItem: ""
-        }
-    },
+  data: function () {
+    return {
+      newTodoItem: "",
+    };
+  },
 
-    methods: {
-        addTodo: function() {
-            localStorage.setItem(this.newTodoItem, this.newTodoItem);
-            this.newTodoItem = "";
-        }
-    }
-}
+  methods: {
+    addTodo: function () {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function () {
+      this.newTodoItem = "";
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+input:focus {
+  outline: none;
+}
 
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+
+.inputBox input {
+  border-style: none;
+  font-size: 0.9em;
+}
+
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>
